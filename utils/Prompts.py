68a3352019079ccadca8c914ca34b9ae50cac5e6
KylePsilopoulos <99224018+KylePsilopoulos@users.noreply.github.com>
2023-11-04 11:55:06 -0700

    Add files via upload
    Templates for LLMs

---
+++ utils/Prompts.py
@@ -0,0 +1,102 @@
+patient_template = """You are a patient generator for simulating {gender} medical patients with {disease_state}.
+                    You will use the disease context below to help you create a {gender} virtual medical patient with 
+                    {disease_state}. The virtual patient has not been diagnosed with {disease_state} yet, so do not 
+                    include {disease_state} or the words related to {disease_state} in patient history or any of the 
+                    fields below. Try to be very creative when creating the patient,but try to be very accurate when 
+                    creating medical information.
+
+                    Disease Context: {context}
+
+                    Input your generated answer within each set of <>, and return all of the following fields exactly as 
+                    formatted below:
+                    Name: <insert random made up name here, try to be creative>
+                    Age: <insert random integer value between 18 and 100 here>
+                    Race/Ethnicity: <insert random race/ethnicity here>
+                    Gender: <insert {gender} here>
+                    Height: <insert random human height value in feet and inches here>
+                    Weight: <insert random human weight value in pounds here>
+                    Blood pressure: <insert appropriate value in mm Hg here>
+                    Heart Rate: <insert appropriate value in beats per minute here>
+                    Pain Level: <insert value between 0-9 here>
+                    Patient History: <insert made up medical history here, but do not say or include the words 
+                    {disease_state}>
+                    Family History: <insert made up family history here, potentially related to {disease_state} but do 
+                    not say or include the words {disease_state}>
+                    Current signs and symptoms: <insert 2 to 6 signs/symptoms found in {context} but do not say or 
+                    include the words '{disease_state}'>
+                    Current Medications: <insert a few OTC or prescription medications but do not include any 
+                    medications that are used to treat {disease_state}>
+                    Additional Notes: <add some additional details to make the patient seem realistic, but do not say or 
+                    include the words '{disease_state}'>
+                    """
+
+physical_template = """You are generating a mock physical findings report for {gender} medical patients with 
+                    {disease_state}. You will use the patient template and physical evaluation context below to fill out 
+                    a list for each of the required information for this specific {gender} virtual medical patient with 
+                    {disease_state}.
+
+                    Patient template: {patient_template}
+
+                    Physical Context: {context}
+
+                    An example output is provided below for a random disease. Do not copy this information, just use it 
+                    as a style reference:
+                    Physical Exam Findings: red, pustular rash on upper right arm.
+
+                    Input your generated answer within each set of <>, and return all of the following required fields 
+                    exactly as formatted below:
+                    Physical Exam Findings: <insert one to three physical exam findings found in {context}, do not say 
+                    or include the words '{disease_state}', list none if there are none in the {context}, make sure this 
+                    doesn't contradict the patient template>
+                    """
+
+diagnostic_template = """You are generating a mock diagnostic findings report for {gender} medical patients with 
+                      {disease_state}. You will use the patient template and diagnostic evaluation context below to fill 
+                      out a list for each of the required information for this specific {gender} virtual medical patient 
+                      with {disease_state}.
+
+                      Patient template: {patient_template}
+
+                      Diagnostic Findings Context: {context}
+
+                      An example output is provided below for a random disease. Do not copy this information, just use 
+                      it as a style reference:
+                      Diagnostic Exam Findings: positive sensitivity to latex and cosmetics allergen tests
+
+
+                      Input your generated answer within each set of <>, and return all of the following required fields 
+                      exactly as formatted below:
+                      Diagnostic Exam Findings: <insert one to three diagnostic exam findings found in {context}, do not 
+                      say or include the words '{disease_state}', list none if there are none in the {context}, make 
+                      sure this doesn't contradict the patient template>
+                      """
+
+treatment_template = """You are a medical doctor that is develops treatment plans for {gender} medical patients with 
+                     {disease_state}. You will use the treatment context below to help you create a treatment plan for 
+                     a specific patient, whose information can be found in the patient context below. Make sure the 
+                     treatment plan includes the best pharmacological and non-pharmacological treatment options for this 
+                     patient and their {disease_state}.
+
+                     Treatment Context: {context}
+
+                     Patient Context: {patient}
+
+                     Treatment plan:
+                     """
+
+labs_template = """You are a lab value generator for simulating {gender} medical patients with {disease_state}. You will 
+                generate a lab value for {lab} seen in patients with {disease_state}, if the {lab} value is typically 
+                affected by {disease_state}, generate a believable value outside of the normal range. For example, if a 
+                patient has hyponatremia, you will generate a sodium electrolyte value below the normal range. If {lab} 
+                values are not typically affected by {disease_state} return a value within the normal range. Use the 
+                provided normal range {lab} values provided below to determine normal and abnormal values.
+
+                Normal {lab} values: {context}
+
+                Return the values to the user in the following format:
+                {lab}: <insert {lab} value here, make sure it is an appropriate value for a patient with 
+                {disease_state}, if info from Normal {lab} values is not relevant then choose a value for {lab} to the 
+                best of your ability>
+
+                Patient Lab Value:
+                """
